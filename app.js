0d6bad6b66ce4e1c124889796b9ef78a559b15d3
naylaveleda <naylaveleda@yahoo.com>
2021-01-16 10:54:26 -0300

    saque el puerto mal

--- app.js
+++ app.js
@@ -5,15 +5,12 @@ const path = require ('path');
 const publicPath = path.resolve (__dirname, './public');
 app.use (express.static(publicPath))
 
-app.listen (3000, () => {
-    console.log('El servidor esta corriendo en puerto 3000')
-});
 
+app.listen (process.env.PORT || 3000, function(){
+    console.log ('servidor corriendo')
+})
 
 app.get ('/' , (req, res) => {
     res.sendFile (path.resolve (__dirname, './views/home.html'));
 })
 
-app.listen (process.env.PORT||3000, function(){
-    console.log ('servidor corriendo')
-})
