3cb0b883aae8f85ca066875f4c98e020ed72d456
naylaveleda <naylaveleda@yahoo.com>
2021-01-17 20:31:36 -0300

    mejore el header y main

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const app = express ();
 const path = require ('path');
 
 const publicPath = path.resolve (__dirname, './public');
-app.use (express.static(publicPath))
+app.use (express.static(publicPath));
 
 
 app.listen (process.env.PORT || 3000, function(){
     console.log ('servidor corriendo')
-})
+});
 
 app.get ('/' , (req, res) => {
     res.sendFile (path.resolve (__dirname, './views/home.html'));
-})
+});
 
